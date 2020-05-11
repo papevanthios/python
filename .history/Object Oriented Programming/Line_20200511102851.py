2deade114c48ac8855cc05161a3c97f7a5d22197
Evanthios Papadopoulos <papevanthios@gmail.com>
2020-05-11 10:30:06 +0300

    11/5/20third

---
+++ .history/Object Oriented Programming/Line_20200511102851.py
@@ -0,0 +1,19 @@
+import math 
+from .. import Header1
+class Line:
+    
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        print(((x2-x1)**2 + (y2-y1)**2)**0.5)
+
+
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
+li.distance()
