fdfc20b8f576a27bb2df3f5f47327025c1573314
Evanthios Papadopoulos <papevanthios@gmail.com>
2020-05-16 16:08:56 +0300

    e programm

---
+++ .history/List of Capstone Projects/find_e_to_the_nth_digit_20200516160556.py
@@ -0,0 +1,25 @@
+'''
+Find E to the Nth Digit - Just like the previous problem, but with e instead of π (pi). Enter a number and have the program generate e up to that many decimal places. Keep a limit to how far the program will go.
+'''
+
+import HeaderOfFiles
+import math
+
+def e_func(number):
+    '''
+    Generate E to the given number.
+    '''
+    return round(math.pi, number)
+
+while True:
+    try:
+        e = int(input("Enter a number between 0 to 15 for E programm: "))
+        if  e > 15:
+            print("Give a number between 0 and 15 please!")
+            continue
+        break
+    except ValueError:
+        print("Give me a number please!")
+
+# print(e_func(p))
+print(math.e)
